8d2fe1db602b245abac79df5d61fefc7a9de5852
ArkLotif <arklotif@gmail.com>
2016-02-07 15:06:57 -0700

    Border

--- Config.js
+++ Config.js
@@ -1,5 +1,6 @@
-var FontSize = 200;
+var FontSize = 200; // In pixels
 var TwelveHour = true;
-var TopPadding = 500;
-var TextColor = "ff0000";
-var Opacity = 0.8;
+var TopPadding = 500; // In pixels
+var TextColor = "000000"; // Hex code
+var Opacity = 0.8; // 0.0 - 1.0
+var BorderWidth = 10;
