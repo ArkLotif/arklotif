bb7d9d94009ab3595079c740c03c996a03fa45f1
ArkLotif <arklotif@gmail.com>
2016-02-10 04:31:53 -0700

    text outline

--- Config.js
+++ Config.js
@@ -1,5 +1,5 @@
 var FontSize = 200; // In pixels
 var TwelveHour = true;
 var TopPadding = 500; // In pixels
-var TextColor = "000000"; // Hex code
-var Opacity = 0.8; // 0.0 - 1.0
+var TextColor = "ffffff"; // Hex code
+var Opacity = 1.0; // 0.0 - 1.0
