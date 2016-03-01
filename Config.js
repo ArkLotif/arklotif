2230fa1c8b174dc71fcf81a16e5bec29b936cb84
ArkLotif <arklotif@gmail.com>
2016-02-29 17:45:16 -0700

    final

--- Config.js
+++ Config.js
@@ -1,5 +1,5 @@
 var FontSize = 144; // In pixels
 var TopPadding = 200; // In pixels
 var TwelveHour = true;
-var TextColor = "000000"; // Hex code
+var TextColor = "ffffff"; // Hex code
 var Opacity = 1.0; // 0.0 - 1.0
