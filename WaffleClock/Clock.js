2853e68fcce172c9e01e81f37d2faa6bdd61acf2
ArkLotif <arklotif@gmail.com>
2016-05-14 15:01:27 -0600

    WaffleClock

---
+++ WaffleClock/Clock.js
@@ -0,0 +1,27 @@
+src="Config.js"
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    if (TwelveHour == true) {h = TwelveTime(h)};
+    var t = setTimeout(startTime, 500);
+    document.getElementById("txt").innerHTML = h + ":" + m;
+    document.getElementById("txt").style.paddingTop = TopPadding + "px";
+    document.getElementById("txt").style.fontSize = FontSize + "px";
+    document.getElementById("txt").style.color = "#" + TextColor;
+    document.getElementById("txt").style.opacity = Opacity;
+}
+
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};
+    return i;
+}
+
+function TwelveTime(h) {
+    if (h >= 12) {h = h - 12};
+    if (h == 0) {h = 12};
+    return h;
+}
---
+++ WaffleClock/Config.js
@@ -0,0 +1,5 @@
+var FontSize = 72; // In pixels
+var TopPadding = 200; // In pixels
+var TwelveHour = true;
+var TextColor = "ffffff"; // Hex code
+var Opacity = 1.0; // 0.0 - 1.0
