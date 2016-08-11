a1463cbe69cda19817560871d22d0efff5410e82
ArkLotif <arklotif@gmail.com>
2016-08-10 22:40:10 -0600

    month

--- Clock/Clock.js
+++ Clock/Clock.js
@@ -27,27 +27,26 @@ function startTime() {
     var month = today.getMonth();
     var date = today.getDate();
     
-    if (day == 0) {
-        day = "Sunday,";
-    }
-    if (day == 1) {
-        day = "Monday,";
-    }
-    if (day == 2) {
-        day = "Tuesday,";
-    }
-    if (day == 3) {
-        day = "Wednesday,";
-    }
-    if (day == 4) {
-        day = "Thursday,";
-    }
-    if (day == 5) {
-        day = "Friday,";
-    }
-    if (day == 6) {
-        day = "Saturday,";
-    }
+    if (day == 0) {day = "Sunday,";}
+    if (day == 1) {day = "Monday,";}
+    if (day == 2) {day = "Tuesday,";}
+    if (day == 3) {day = "Wednesday,";}
+    if (day == 4) {day = "Thursday,";}
+    if (day == 5) {day = "Friday,";}
+    if (day == 6) {day = "Saturday,";}
+    
+    if (month == 0) {month = "January";}
+    if (month == 1) {month = "February";}
+    if (month == 2) {month = "March";}
+    if (month == 3) {month = "April";}
+    if (month == 4) {month = "May";}
+    if (month == 5) {month = "June";}
+    if (month == 6) {month = "July";}
+    if (month == 7) {month = "August";}
+    if (month == 8) {month = "September";}
+    if (month == 9) {month = "October";}
+    if (month == 10) {month = "November";}
+    if (month == 11) {month = "December";}
     
     document.getElementById('Day').innerHTML = day;
     document.getElementById('Month').innerHTML = month;
